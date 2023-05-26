ab92d312f2615f7e983ac635c0eb8af14a7edd88
panuruj <panuruj.ba@brightexpert.co>
2023-05-26 14:46:54 +0700

    add CORS config

--- main.go
+++ main.go
@@ -4,11 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/panurujz/calculate-term-sheet/services"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.PATCH, echo.POST, echo.DELETE},
+	}))
 
 	e.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Server is smooth.")
